ec6625f0211edd7b6c7d3df36d0e94b93f3ab9a8
Markus Wallerberger <mwallerb@umich.edu>
2021-09-21 18:51:36 +0200

    Add tests for adapter.

--- src/irbasis3/adapter.py
+++ src/irbasis3/adapter.py
@@ -74,7 +74,10 @@ class Basis:
 
     def compute_unl(self, n, whichl=None):
         """Compute transformation matrix from IR to Matsubara frequencies"""
-        return _select(self._u, whichl).hat(n)
+        n = _np.asarray(n)
+        freq = {'F': 'odd', 'B': 'even'}[self._statistics]
+        uhat = _select(self._u, whichl).hat(freq)
+        return uhat(2 * n + uhat.zeta)
 
     def num_sections_x(self):
         "Number of sections of piecewise polynomial representation of u_l(x)"
--- test/test_compare.py
+++ test/test_compare.py
@@ -45,3 +45,44 @@ def test_svals(stat, lambda_, adapters, references):
 
     np.testing.assert_allclose(adapt.sl(shared), ref.sl(shared),
                                atol=10 * ref.sl(0) * eps, rtol=0)
+
+@pytest.mark.parametrize("stat,lambda_", COMPARE_PARAMS)
+def test_vly(stat, lambda_, adapters, references):
+    adapt = adapters[stat, lambda_]
+    ref = references[stat, lambda_]
+    shared_dim = min(adapt.dim(), ref.dim())
+
+    y = [-1., -.5, -.1, -.01, -.001, -.0001, 0, .0001, .001, .01, .1, .5, 1.]
+    for li in [0, 1, 2, shared_dim//2, shared_dim//2 + 1, shared_dim-1]:
+        uly_adapt = adapt.vly(li, y)
+        uly_ref = ref.vly(li, y)
+        tol = 1e-10 * np.abs(uly_ref).max() * ref.sl(0) / ref.sl(li)
+        np.testing.assert_allclose(uly_adapt, uly_ref, atol=tol, rtol=0)
+
+
+@pytest.mark.parametrize("stat,lambda_", COMPARE_PARAMS)
+def test_ulx(stat, lambda_, adapters, references):
+    adapt = adapters[stat, lambda_]
+    ref = references[stat, lambda_]
+    shared_dim = min(adapt.dim(), ref.dim())
+
+    x = [-1., -.9999, -.999, -.99, -.9, -.5, 0., .5, .9, .99, .999, .9999, 1.]
+    for li in [0, 1, 2, shared_dim//2, shared_dim//2 + 1, shared_dim-1]:
+        uly_adapt = adapt.ulx(li, x)
+        uly_ref = ref.ulx(li, x)
+        tol = 1e-12 * np.abs(uly_ref).max() * ref.sl(0) / ref.sl(li)
+        np.testing.assert_allclose(uly_adapt, uly_ref, atol=tol, rtol=0)
+
+
+@pytest.mark.parametrize("stat,lambda_", COMPARE_PARAMS)
+def test_ulx(stat, lambda_, adapters, references):
+    adapt = adapters[stat, lambda_]
+    ref = references[stat, lambda_]
+    shared_dim = min(adapt.dim(), ref.dim())
+
+    n = [-20, -2, -1, 0, 1, 2, 20, 100, 300, 1000]
+    for li in [0, 1, 2, shared_dim//2, shared_dim//2 + 1, shared_dim-1]:
+        ulxhat_adapt = adapt.compute_unl(n, li)
+        ulxhat_ref = ref.compute_unl(n, li).ravel()
+        tol = 1e-13 * np.abs(ulxhat_ref).max() * ref.sl(0) / ref.sl(li)
+        np.testing.assert_allclose(ulxhat_adapt, ulxhat_ref, atol=tol, rtol=0)
