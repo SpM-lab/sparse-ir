b1ae1ea8273d3b09f623f98c89aa9199a59cd2b6
Markus Wallerberger <mwallerb@umich.edu>
2021-11-15 14:14:49 +0100

    Bump version.

--- src/irbasis3/__init__.py
+++ src/irbasis3/__init__.py
@@ -4,7 +4,7 @@ Intermediate representation (IR) for many-body propagators
 """
 __copyright__ = "Copyright (C) 2020-2021 Markus Wallerberger and others"
 __license__ = "MIT"
-__version__ = "3.0-alpha3"
+__version__ = "3.0-alpha5"
 
 from .kernel import KernelFFlat, KernelBFlat
 from .sve import compute as compute_sve
