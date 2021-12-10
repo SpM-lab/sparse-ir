690d60abc712401716dd76479c941a7d32e295e7
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-12-10 13:50:20 +0900

    Implemented Legendre basis

---
+++ src/irbasis3/augmentation.py
@@ -0,0 +1,41 @@
+import numpy as np
+from .poly import PiecewiseLegendrePoly
+
+class LegendreBasis(object):
+    """Legendre basis
+
+    G(tau) = \sum_{l=0} \sqrt{2l+1} P_l[x(\tau)] G_l/beta,
+    where P_l[x] is the $l$-th Legendre polynomial.
+
+    The basis functions are defined by
+        U_l(\tau) \equiv \sqrt{2l+1} P_l[x(\tau)]/beta.
+
+    Ref: L. Boehnke et al., PRB 84, 075145 (2011)
+    """
+    def __init__(self, statistics, beta, size):
+        if statistics not in 'BF':
+            raise ValueError("Statistics must be either 'B' for bosonic"
+                             "or 'F' for fermionic")
+        if not (beta > 0):
+            raise ValueError("inverse temperature beta must be positive")
+        if not (size > 0):
+            raise ValueError("size of basis must be positive")
+
+        self.statistics = statistics
+        self.beta = beta
+        self.size = size
+
+        # self.u
+        knots = np.array([0, beta])
+        data = np.zeros((size, knots.size-1, size))
+        for l in range(size):
+            data[l, 0, l] = np.sqrt(2*l+1)/beta
+        self.u = PiecewiseLegendrePoly(data, knots)
+
+        # self.uhat
+        # Hack: See basis.py
+        uhat_base = PiecewiseLegendrePoly(np.sqrt(beta) * self.u.data, np.array([-1,1]))
+        self.uhat = uhat_base.hat({'F': 'odd', 'B': 'even'}[statistics])
+
+        # self.v
+        self.v = None
---
+++ test/test_augmentation.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2020-2021 Markus Wallerberger and others
+# SPDX-License-Identifier: MIT
+import numpy as np
+from irbasis3 import augmentation
+from scipy.special import eval_legendre, spherical_jn
+
+import pytest
+
+def compute_Tnl(vsample, n_legendre):
+    """
+    Compute transformation matrix from Legendre to fermionic/bosonic Matsubara frequency
+    Implement Eq. (4.5) in the Boehnke's  Ph.D thesis
+    """
+    Tnl = np.zeros((vsample.size, n_legendre), dtype=np.complex128)
+    for idx_n, v in enumerate(vsample):
+        abs_v = abs(v)
+        sph_jn = np.array(
+            [spherical_jn(l, 0.5*abs_v*np.pi) for l in range(n_legendre)])
+        for il in range(n_legendre):
+            Tnl[idx_n, il] = (1J**(abs_v+il)) * np.sqrt(2*il + 1.0) * sph_jn[il]
+        if v < 0:
+            Tnl[idx_n, :] = Tnl[idx_n, :].conj()
+    return Tnl
+
+@pytest.mark.parametrize("stat", ["F", "B"])
+def test_legenre(stat):
+    beta = 2.0
+    Nl = 10
+    basis = augmentation.LegendreBasis(stat, beta, Nl)
+
+    tau = np.array([0, 0.4*beta, beta])
+    uval = basis.u(tau)
+    for l in range(Nl):
+        np.testing.assert_allclose(
+            uval[l,:],
+            (np.sqrt(2*l+1)/beta) * eval_legendre(l, 2*tau/beta-1)
+            )
+    
+    zeta = {"F": 1, "B": 0}[stat]
+    w = 2*np.arange(-10, 10) + zeta
+
+    uhat_val = basis.uhat(w)
+    uhat_val_ref = compute_Tnl(w, Nl)
+    np.testing.assert_allclose(uhat_val.T, uhat_val_ref)
+
