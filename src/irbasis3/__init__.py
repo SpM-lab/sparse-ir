d56fc7456ced1e3c5c65c084b3c929b15756a2c6
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-12-18 14:50:30 +0900

    Version bump to v3.0-alpha6

--- src/irbasis3/__init__.py
+++ src/irbasis3/__init__.py
@@ -4,7 +4,7 @@ Intermediate representation (IR) for many-body propagators
 """
 __copyright__ = "Copyright (C) 2020-2021 Markus Wallerberger and others"
 __license__ = "MIT"
-__version__ = "3.0-alpha5"
+__version__ = "3.0-alpha6"
 
 from .kernel import KernelFFlat, KernelBFlat
 from .sve import compute as compute_sve
