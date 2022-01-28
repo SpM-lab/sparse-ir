4cde5ce510277b0fe875499c7a43d29bb32b3816
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2022-01-28 12:01:34 +0100

    Tighten whitespace checks.

--- test/test_whitespace.py
+++ test/test_whitespace.py
@@ -1,29 +1,40 @@
 import os
 
 HEREPATH = os.path.abspath(os.path.dirname(__file__))
-SRCDIR = os.path.join(HEREPATH, os.path.pardir, "src", "sparse_ir")
+ROOTDIR = os.path.abspath(os.path.join(HEREPATH, os.path.pardir))
+SRCDIR = os.path.join(ROOTDIR, "src", "sparse_ir")
 
 
 def check_whitespace(files):
     errors = []
+    blank = 0
+    lineno = 0
+    line = ""
+    def add_error(fmt, *params):
+        errors.append((fname, lineno, line, fmt.format(*params)))
+
     for fname in files:
         with open(fname, "r") as file:
+            line = ""
             for lineno, line in enumerate(file, start=1):
-                def add_error(msg):
-                    errors.append((fname, lineno, line, msg))
-                if not line:
-                    break
-                if line[-1] != '\n':
+                if line[-1:] != '\n':
                     add_error("file must end in blank line")
                 line = line[:-1]
+                if line:
+                    blank = 0
+                else:
+                    blank += 1
                 if line[-1:] == '\r':
-                    add_error("file must only have unix line eendings")
+                    add_error("file must only have unix line endings")
                 if line[-1:] == ' ':
                     add_error("line ends in whitespace")
                 if '\t' in line:
                     add_error("line contains tab characters")
                 if len(line) > 90:
-                    add_error("line is too long: {:d} chars".format(len(line)))
+                    add_error("line is too long: {:d} chars", len(line))
+            # end of file
+            if blank != 0:
+                add_error("file has {:d} superflouos blank lines", blank)
 
     msg = ""
     for fname, lineno, line, lmsg in errors:
@@ -44,3 +55,7 @@ def test_ws_testdir():
 
 def test_ws_srcdir():
     check_whitespace(python_files(SRCDIR))
+
+
+def test_ws_setup():
+    check_whitespace(python_files(ROOTDIR))
