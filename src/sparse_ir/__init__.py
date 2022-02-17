7884b3cd30135ef846aa69ad5730340b22cad5f2
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2022-02-17 17:20:05 +0100

    Bump version.

--- src/sparse_ir/__init__.py
+++ src/sparse_ir/__init__.py
@@ -4,7 +4,7 @@ Intermediate representation (IR) for many-body propagators
 """
 __copyright__ = "2020-2021 Markus Wallerberger and others"
 __license__ = "MIT"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 min_xprec_version = "1.0"
 
