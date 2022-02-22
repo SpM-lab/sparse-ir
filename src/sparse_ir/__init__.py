0ccf1fc856880831a6b675e0434266534a050356
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-02-23 07:35:58 +0900

    Bring finite_temp_bases to the top namespace

--- src/sparse_ir/__init__.py
+++ src/sparse_ir/__init__.py
@@ -19,5 +19,5 @@ except ImportError:
 
 from .kernel import RegularizedBoseKernel, LogisticKernel
 from .sve import compute as compute_sve
-from .basis import IRBasis, FiniteTempBasis
+from .basis import IRBasis, FiniteTempBasis, finite_temp_bases
 from .sampling import TauSampling, MatsubaraSampling
