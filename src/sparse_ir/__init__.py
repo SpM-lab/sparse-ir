cb13729ea344ce3660f7d8edcd37f076ce6b56e5
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-10 07:14:13 +0000

    Bump version.

--- src/sparse_ir/__init__.py
+++ src/sparse_ir/__init__.py
@@ -4,7 +4,7 @@ Intermediate representation (IR) for many-body propagators
 """
 __copyright__ = "2020-2021 Markus Wallerberger and others"
 __license__ = "MIT"
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 min_xprec_version = "1.0"
 
