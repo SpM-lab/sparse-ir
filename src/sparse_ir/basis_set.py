b1def7b43be9b6e63e83b69c9ccba81b05d77ce4
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-17 07:27:31 +0000

    Add a class for holding IR bases for fermion and boson as well as associated sampling objects

---
+++ src/sparse_ir/basis_set.py
@@ -0,0 +1,38 @@
+from .basis import FiniteTempBasis, finite_temp_bases
+from .sampling import TauSampling, MatsubaraSampling
+
+
+class FiniteTempBasisSet:
+    """Class for holding IR bases and sparse-sampling objects
+
+    An object of this class holds IR bases for fermion and bosons
+    and associated sparse-sampling objects
+    """
+    def __init__(self, beta, wmax, eps, sve_result=None):
+        """
+        Create basis sets for fermion and boson and
+        associated sampling objects.
+        Fermion and bosonic bases are constructed by SVE of the logistic kernel.
+        """
+        if sve_result is None:
+            # Create bases by sve of the logistic kernel
+            self.basis_f, self.basis_b = finite_temp_bases(beta, wmax, eps)
+        else:
+            # Create bases using the given sve results
+            self.basis_f = FiniteTempBasis("F", beta, wmax, eps, sve_result=sve_result)
+            self.basis_b = FiniteTempBasis("B", beta, wmax, eps, sve_result=sve_result)
+
+        self.beta = beta
+        self.wmax = wmax
+        self.eps = eps
+
+        # Tau sampling
+        self.smpl_tau_f = TauSampling(self.basis_f)
+        self.smpl_tau_b = TauSampling(self.basis_b)
+        self.tau = self.smpl_tau_f.sampling_points
+
+        # Matsubara sampling
+        self.smpl_wn_f = MatsubaraSampling(self.basis_f)
+        self.smpl_wn_b = MatsubaraSampling(self.basis_b)
+        self.wn_f = self.smpl_wn_f.sampling_points
+        self.wn_b = self.smpl_wn_b.sampling_points
