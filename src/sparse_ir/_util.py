c6318374079feaa912b9d30ed6038410c76aa692
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2022-05-17 12:09:38 +0200

    Better decorator.

--- src/sparse_ir/_util.py
+++ src/sparse_ir/_util.py
@@ -5,17 +5,33 @@ import numpy as np
 
 
 def ravel_argument(last_dim=False):
-    """Guarantees to pass a ravelled array into the method"""
-    def ravel_argument_decorator(inner_fn):
-        @functools.wraps(inner_fn)
-        def ravelled_func(self, x):
-            x = np.asarray(x)
-            x_flat = x.ravel()
-            res = inner_fn(self, x_flat)
-            if last_dim:
-                return res.reshape(res.shape[:-1] + x.shape)
-            else:
-                return res.reshape(x.shape + res.shape[1:])
+    """Wrap function operating on 1-D numpy array to allow arbitrary shapes.
 
-        return ravelled_func
-    return ravel_argument_decorator
+    This decorator allows to write functions which only need to operate over
+    one-dimensional (ravelled) arrays.  This often simplifies the "shape logic"
+    of the computation.
+    """
+    return lambda fn: RavelArgumentDecorator(fn, last_dim)
+
+
+class RavelArgumentDecorator(object):
+    def __init__(self, inner, last_dim=False):
+        self.instance = None
+        self.inner = inner
+        self.last_dim = last_dim
+        functools.update_wrapper(self, inner)
+
+    def __get__(self, instance, _owner=None):
+        self.instance = instance
+        return self
+
+    def __call__(self, x):
+        x = np.asarray(x)
+        if self.instance is None:
+            res = self.inner(x.ravel())
+        else:
+            res = self.inner(self.instance, x.ravel())
+        if self.last_dim:
+            return res.reshape(res.shape[:-1] + x.shape)
+        else:
+            return res.reshape(x.shape + res.shape[1:])
