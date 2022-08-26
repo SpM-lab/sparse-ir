b73f7c3c3dff9e2966719fc4992ace5806b767b7
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-08-26 12:50:42 +0000

    Fix too long lines

--- src/sparse_ir/dlr.py
+++ src/sparse_ir/dlr.py
@@ -13,21 +13,24 @@ from . import svd
 class DiscreteLehmannRepresentation(abstract.AbstractBasis):
     """
     Discrete Lehmann representation (DLR) with poles selected according to extrema of IR.
-    
-    This class implements a variant of the discrete Lehmann representation (`DLR`_) [1].  Instead
-    of a truncated singular value expansion of the analytic continuation kernel ``K`` like the IR,
-    the discrete Lehmann representation is based on a "sketching" of ``K``.  The resulting basis
-    is a linear combination of discrete set of poles on the real-frequency axis, continued to the
-    imaginary-frequency axis:
-    
+
+    This class implements a variant of the discrete Lehmann representation (`DLR`_) [1].
+    Instead of a truncated singular value expansion of the analytic continuation
+    kernel ``K`` like the IR, the discrete Lehmann representation is
+    based on a "sketching" of ``K``.  The resulting basis is a linear combination of
+    discrete set of poles on the real-frequency axis,
+    continued to the imaginary-frequency axis:
+
          G(iv) == sum(a[i] / (iv - w[i]) for i in range(L))
-    
+
     Warning
-        The poles on the real-frequency axis selected for the DLR are based on a rank-revealing
-        decomposition, which offers accuracy guarantees.  Here, we instead select the pole locations
-        based on the zeros of the IR basis functions on the real axis, which is a heuristic.  We do not
-        expect that difference to matter, but please don't blame the DLR authors if we were wrong :-)
-    
+        The poles on the real-frequency axis selected for the DLR are based
+        on a rank-revealing decomposition, which offers accuracy guarantees.
+        Here, we instead select the pole locations
+        based on the zeros of the IR basis functions on the real axis,
+        which is a heuristic. We do not expect that difference to matter,
+        but please don't blame the DLR authors if we were wrong :-)
+
     [1]: https://doi.org/10.48550/arXiv.2110.06765
     """
     def __init__(self, basis: _basis.FiniteTempBasis, sampling_points=None):
