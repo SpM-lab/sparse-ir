35a688e6532b0f87d282f5b0e6e656e3bfcf94cf
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-06-12 22:53:49 +0900

    Update workflows & bump to v1.1.7

--- src/sparse_ir/__init__.py
+++ src/sparse_ir/__init__.py
@@ -12,7 +12,7 @@ intermediate representation of correlation functions.  It provides:
 """
 __copyright__ = "2020-2025 Markus Wallerberger, Hiroshi Shinaoka, and others"
 __license__ = "MIT"
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 
 from .kernel import RegularizedBoseKernel, LogisticKernel
 from .sve import compute as compute_sve, SVEResult
